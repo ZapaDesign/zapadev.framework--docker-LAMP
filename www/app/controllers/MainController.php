8dfe48fca1ed418912b1417542c592178c5e9354
zapa.poltava@gmail.com <aa|2NmTxM+72N3dD-jEh>
2022-02-14 13:59:37 +0200

    Add methods to Model

--- www/app/controllers/MainController.php
+++ www/app/controllers/MainController.php
@@ -14,6 +14,13 @@ class MainController extends AppController
         $model = new Main;
         $posts = $model->findAll("SELECT * FROM posts");
 
+//        $post = $model->findOne(2);
+//        $data = $model->findBySql("SELECT * FROM {$model->table} ORDER BY id DESC LIMIT 2");
+//        $data = $model->findBySql("SELECT * FROM {$model->table} WHERE title LIKE ?", ['%JQuery%']);
+        $data = $model->findLike('JQ', 'title');
+
+        debug($data);
+
         $title = 'PAGE TITLE';
         $this->set([
             'title' => $title,
